3f7a3c4c6242a48ae9bfb1911190da3f70f64930
ANSSoftware <77612185+ANSSoftware@users.noreply.github.com>
2021-06-12 15:59:10 +0300

    1. Ister Halledildi

--- app/src/main/java/com/ans/borsa/emirler.kt
+++ app/src/main/java/com/ans/borsa/emirler.kt
@@ -19,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_emirler.*
 class emirler : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
+
     // arrayler tanımlandı
     var urunMiktariFromFB: ArrayList<String> = ArrayList()
     var urunOnayiFromFB: ArrayList<String> = ArrayList()
@@ -28,8 +29,6 @@ class emirler : AppCompatActivity() {
     var emirler: emirlerRA? = null
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emirler)
@@ -37,7 +36,13 @@ class emirler : AppCompatActivity() {
         db = FirebaseFirestore.getInstance()
         var layoutManagerEmiler = LinearLayoutManager(this)
         emirlerRV.layoutManager = layoutManagerEmiler
-        emirler = emirlerRA(urunFromFB,urunOnayiFromFB,urunMiktariFromFB,UserEmailFromFB,urunTutariFromFB)
+        emirler = emirlerRA(
+            urunFromFB,
+            urunOnayiFromFB,
+            urunMiktariFromFB,
+            UserEmailFromFB,
+            urunTutariFromFB
+        )
         emirlerRV.adapter = emirler
         getDataFromFSEmirler()
     }
@@ -46,7 +51,11 @@ class emirler : AppCompatActivity() {
 
         db.collection("Emirler").addSnapshotListener { snapshot, exception ->
             if (exception != null) {
-                Toast.makeText(applicationContext, exception.localizedMessage.toString(), Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    applicationContext,
+                    exception.localizedMessage.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
                 if (snapshot != null) {
                     if (!snapshot.isEmpty) {
@@ -57,30 +66,28 @@ class emirler : AppCompatActivity() {
                         urunOnayiFromFB.clear()
                         var documents = snapshot.documents
                         for (document in documents) {
-                            val urunKG = document.get("UrunKG") as Number
-                            val urunTutari = document.get("EmirTutar") as Number
-                            val userEmail = document.get("UserEmail") as String
-                            val urun = document.get("Urun") as String
-                            val urunOnay = document.get("EmirOnay") as Boolean
+                            var urunKG = document.get("UrunKG") as Number
+                            var urunTutari = document.get("EmirTutar") as Number
+                            var userEmail = document.get("UserEmail") as String
+                            var urun = document.get("Urun") as String
+                            var urunOnay = document.get("EmirOnay") as Boolean
 
-                            val urunMiktariText = urunKG.toString() + "KG"
-                            val urunTutariText = urunTutari.toString() + "TL"
-                            val urunOnayText = urunOnay.toString()
+                            var urunMiktariText = urunKG.toString() + "KG"
+                            var urunTutariText = urunTutari.toString() + "TL"
 
-                                if(userEmail == auth.currentUser!!.email.toString()){
+                            if (userEmail == auth.currentUser!!.email.toString() ) {
+                                if (urunOnay == false) {
+
                                     UserEmailFromFB.add(userEmail)
                                     urunMiktariFromFB.add(urunMiktariText)
                                     urunTutariFromFB.add(urunTutariText)
                                     urunFromFB.add(urun)
-                                    if(urunOnayText=="false"){
-                                        urunOnayiFromFB.add("Emriniz Onaylanmadı")
-                                    }else if(urunOnayText=="true"){
-                                        urunOnayiFromFB.add("Emriniz Onaylandı")
-                                        getDFSatilanUrunler(urunKG,urunTutari,userEmail,urun)
-                                    }
+                                    getDFSatilanUrunler(urunKG, urunTutari, userEmail, urun)
+                                    urunOnayiFromFB.add("Emriniz Bekliyor")
 
-                                    emirler!!.notifyDataSetChanged()
                                 }
+                                emirler!!.notifyDataSetChanged()
+                            }
                         }
                     }
                 }
@@ -104,7 +111,7 @@ class emirler : AppCompatActivity() {
         } else if (item.itemId == R.id.anasayfa) {
             val intent = Intent(applicationContext, AnaMenu::class.java)
             startActivity(intent)
-        }else if (item.itemId == R.id.bilgigiris) {
+        } else if (item.itemId == R.id.bilgigiris) {
             val intent = Intent(applicationContext, com.ans.borsa.bilgiGiris::class.java)
             startActivity(intent)
         }
@@ -112,85 +119,140 @@ class emirler : AppCompatActivity() {
     }
 
 
-    fun getDFSatilanUrunler(urunKG: Number,urunTutari: Number,userEmail :String,urun :String) {
-        var y=0 //satın al bilgileri rv ye çekiliyor
+    fun getDFSatilanUrunler(urunKG: Number, urunTutari: Number, userEmail: String, urun: String) {
+        var y = 0 //satın al bilgileri rv ye çekiliyor
         db.collection("KullaniciUrunleri").document("KullaniciUrunleriDoc")
-                .collection(urun).addSnapshotListener { snapshot, exception ->
-            if (exception != null) {
-                Toast.makeText(applicationContext, exception.localizedMessage.toString(), Toast.LENGTH_LONG).show()
-            } else {
-                if (snapshot != null) {
-                    if (!snapshot.isEmpty) {
+            .collection(urun).addSnapshotListener { snapshot, exception ->
+                if (exception != null) {
+                    Toast.makeText(
+                        applicationContext,
+                        exception.localizedMessage.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if (snapshot != null) {
+                        if (!snapshot.isEmpty) {
 
-                        var documents = snapshot.documents
-                        for (document in documents) {
-                            val sUrunTutari = document.get("UrunTutari") as Number
-                            val sUserEmail = document.get("UserEmail") as String
-                            if(sUrunTutari==urunTutari && y==0){
-                                y++
-                                emirIsle(urun,sUserEmail,urunKG,urunTutari)
+                            var documents = snapshot.documents
+                            for (document in documents) {
+                                var sUrunTutari = document.get("UrunTutari") as Number
+                                var sUserEmail = document.get("UserEmail") as String
+                                var sUrunMiktari = document.get("UrunMiktari") as Number
+                                var urunTutariD =sUrunTutari.toDouble()
+                                if (urunTutariD == urunTutari && y == 0) {
+                                    y++
+                                    emirIsle(urun, sUserEmail, urunKG, urunTutari)
+                                    emirOnayla(urunKG, urunTutari, urun)
+                                }
+
                             }
-
                         }
                     }
                 }
             }
-        }
+
     }
 
-    fun emirIsle(secilenUrun: String, sUserEmail: String,urunKG: Number,urunTutari: Number) { // satın al butonuna basılırsa eğer bakiyede para varsa satın alma işlemi yapılıyor
-        var y = 0
+    fun emirIsle(
+        secilenUrun: String,
+        sUserEmail: String,
+        urunKG: Number,
+        urunTutari: Number
+    ) { // satın al butonuna basılırsa eğer bakiyede para varsa satın alma işlemi yapılıyor
+        var z = 0
         var saticininKalanUrunu: Number
         var almakIstenenKG = urunKG.toDouble()
-
         db.collection("KullaniciUrunleri").document("KullaniciUrunleriDoc")
-                .collection(secilenUrun).orderBy("UrunTutari", Query.Direction.ASCENDING)
-                .addSnapshotListener { snapshot, exception ->
-                    if (exception != null) {
-                        Toast.makeText(applicationContext, exception.localizedMessage.toString(), Toast.LENGTH_LONG).show()
-                    } else {
-                        if (snapshot != null) {
-                            if (!snapshot.isEmpty) {
-                                var documents = snapshot.documents
+            .collection(secilenUrun).orderBy("UrunTutari", Query.Direction.ASCENDING)
+            .addSnapshotListener { snapshot, exception ->
+                if (exception != null) {
+                    Toast.makeText(
+                        applicationContext,
+                        exception.localizedMessage.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if (snapshot != null) {
+                        if (!snapshot.isEmpty) {
+                            var documents = snapshot.documents
 
-                                for (document in documents) {
-                                    var urunMiktari = document.get("UrunMiktari") as Number
-                                    var UserEmail = document.get("UserEmail") as String
-                                    var urunMiktariD = urunMiktari.toDouble()
+                            for (document in documents) {
+                                var urunMiktari = document.get("UrunMiktari") as Number
+                                var UserEmail = document.get("UserEmail") as String
+                                var urunMiktariD = urunMiktari.toDouble()
 
-                                    if (almakIstenenKG <= urunMiktariD && y == 0  && sUserEmail==UserEmail) {
-                                        y++
-                                        saticininKalanUrunu = urunMiktariD - almakIstenenKG
-                                        db.collection("KullaniciUrunleri").document("KullaniciUrunleriDoc")
-                                                .collection(secilenUrun).document(document.id)
-                                                .update("UrunMiktari", saticininKalanUrunu)
-                                        emiriKaydet(urunKG,urunTutari,secilenUrun)
-                                        break
-                                    }
-
+                                if (almakIstenenKG <= urunMiktariD && z == 0 && sUserEmail == UserEmail) {
+                                    z++
+                                    saticininKalanUrunu = urunMiktariD - almakIstenenKG
+                                    db.collection("KullaniciUrunleri")
+                                        .document("KullaniciUrunleriDoc")
+                                        .collection(secilenUrun).document(document.id)
+                                        .update("UrunMiktari", saticininKalanUrunu)
+                                    emiriKaydet(urunKG, urunTutari, secilenUrun)
+                                    break
                                 }
+
                             }
                         }
                     }
                 }
-
+            }
     }
 
 
-    fun emiriKaydet(urunKGKayit :Number,urunTutarKayit: Number,urunKayit: String){ // Emir verme işlemini yapan kod
+    fun emiriKaydet(
+        urunKGKayit: Number,
+        urunTutarKayit: Number,
+        urunKayit: String
+    ) { // Emir verme işlemini yapan kod
         val postMapKayitUrun = hashMapOf<String, Any>()
         postMapKayitUrun.put("urunKGKayit", urunKGKayit)
         postMapKayitUrun.put("urunTutarKayit", urunTutarKayit)
         postMapKayitUrun.put("urunKayit", urunKayit)
         postMapKayitUrun.put("tarih", Timestamp.now()) //emir bilgileri veri tabanına aktarılıyor
-        db.collection("UrunKayitleri").document(auth.currentUser!!.email.toString()).collection("SatinAlinanUrunler").add(postMapKayitUrun).addOnCompleteListener { task ->
-
+        db.collection("UrunKayitleri").document(auth.currentUser!!.email.toString())
+            .collection("SatinAlinanUrunler").add(postMapKayitUrun).addOnCompleteListener { task ->
             if (task.isComplete && task.isSuccessful) {
             }
         }.addOnFailureListener { exception ->
-            Toast.makeText(applicationContext, exception.localizedMessage.toString(), Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                applicationContext,
+                exception.localizedMessage.toString(),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
+    fun emirOnayla(urunKG: Number, urunTutari: Number, urun: String) {
+        var t = 0
+        db.collection("Emirler").addSnapshotListener { snapshot, exception ->
+            if (exception != null) {
+                Toast.makeText(
+                    applicationContext,
+                    exception.localizedMessage.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                if (snapshot != null) {
+                    if (!snapshot.isEmpty) {
+                        var documents = snapshot.documents
+                        for (document in documents) {
+                            val surunKG = document.get("UrunKG") as Number
+                            val surunTutari = document.get("EmirTutar") as Number
+                            val suserEmail = document.get("UserEmail") as String
+                            val surun = document.get("Urun") as String
+                            // Degiskenlerin baslarindaki s ler satici olduklarini belirtiyor
+                            if (suserEmail == auth.currentUser!!.email.toString() && urunKG == surunKG && urun == surun && surunTutari == urunTutari && t == 0) {
+                                t++
+                                db.collection("Emirler").document(document.id).update("EmirOnay", true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
+
+    }
 }
+
